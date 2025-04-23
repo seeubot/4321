37516cd1a68f87ae986da66f13feee2a1d242691
seeubot <155578467+seeubot@users.noreply.github.com>
2025-04-24 00:05:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,16 @@
-source .venv/bin/activate && xria --enable-rpc --rpc-listen-all=false --rpc-allow-origin-all --daemon --max-tries=50 --retry-wait=3 --continue=true --min-split-size=4M --split=10 --allow-overwrite=true && python3 terabox.py
+#!/bin/bash
+
+# Check if aria2 is running
+if ! pgrep -x "aria2c" > /dev/null
+then
+    echo "Starting aria2c daemon..."
+    aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --daemon
+else
+    echo "aria2c is already running"
+fi
+
+# Patch the code to work with standard Pyrogram
+python patch_pyrogram.py
+
+# Start the bot
+python terabox.py
